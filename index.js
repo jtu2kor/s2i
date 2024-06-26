f003bb641260fd3540bddb6b7be3cb5566523557
jtu2kor <tanay.choudhari@bosch.com>
2024-06-26 13:24:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Tarnished One Changed new!');
+  res.end('It works \o/');
 }).listen(8080); 
 
